312475debd3201707f5e1650a69d8d446b29a92a
Marius Dima <seth2k7@gmail.com>
2014-09-09 08:52:59 +0300

    Initial commit

---
+++ src/test/java/cucumber/bdd/CukesRunner.java
@@ -0,0 +1,17 @@
+package cucumber.bdd;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Setek on 9/9/2014.
+ */
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        format = {"pretty", "html:target/cucumber-html-report"},
+        features = "src/test/resources"
+)
+public class CukesRunner {
+}
